a2866faad1e77dec43e7784d5009b75e4f595011
Francois Chataigner <francois.chataigner@eurecat.org>
2019-06-03 20:10:25 +0200

    support padding to avoid unnecessary copies

--- src/AStar2.cpp
+++ src/AStar2.cpp
@@ -61,7 +61,7 @@ PathFinder::~PathFinder()
 }
 
 
-void PathFinder::setWorldData(unsigned width, unsigned height, const uint8_t *data, uint8_t color_threshold)
+void PathFinder::setWorldData(unsigned width, unsigned height, const uint8_t *data, size_t bytes_per_line, uint8_t color_threshold)
 {
     _obstacle_threshold = color_threshold;
     if( width >= std::numeric_limits<int16_t>::max() ||
@@ -69,14 +69,21 @@ void PathFinder::setWorldData(unsigned width, unsigned height, const uint8_t *da
     {
         throw std::invalid_argument("Either width or height exceed the maximum size allowed (32768) ");
     }
+    
+    if (bytes_per_line==0) bytes_per_line=width;
 
     _world_width  = width;
     _world_height = height;
     _gridmap.resize(width*height);
 
-    for (size_t i=0; i<width*height; i++ )
+    for (size_t row=0; row<height; row++ )
     {
-        _gridmap[i].world = data[i];
+        size_t data_idx = row*bytes_per_line, grid_idx = row*width;
+        
+        for (size_t col=0; col<width*height; col++, data_idx++, grid_idx++ )
+        {
+            _gridmap[grid_idx].world = data[data_idx];
+        }
     }
 }
 
